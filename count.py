8d14c652d5d8fd3220b9d2a4fbf55b3617f0f39f
Rone Chen <m13552079019@163.com>
2024-02-09 14:38:32 +0800

    feat: add new feature

--- count.py
+++ count.py
@@ -1,11 +1,16 @@
-file = open('/Users/ronechen/Ansible/test-projects/baselines/win_baseline/defaults/main.yml', 'r')
-count_1 = 0
-count_2 = 0
-lines = file.readlines()
-for line in lines:
-    if 'Manage' in line:
-        count_1 += 1
-    if 'True' in line:
-        count_2 += 1
+import time
 
-print(f'Number of lines containing tasks: {count_1, count_2}')
+for i in range(100000):
+    file = open('/Users/ronechen/Ansible/test-projects/baselines/win_baseline/defaults/main.yml', 'r')
+    count_1 = 0
+    count_2 = 0
+    lines = file.readlines()
+    for line in lines:
+        if 'Manage' in line:
+            count_1 += 1
+        if 'True' in line:
+            count_2 += 1
+
+    print(f'Number of lines containing tasks: {count_1, count_2}')
+    file.close()
+    time.sleep(60)
