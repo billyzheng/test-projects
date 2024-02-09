1f4d27ea08ba175c5d71eee7d9af4cbc206daa07
Rone Chen <m13552079019@163.com>
2024-02-09 14:52:25 +0800

    feat: add a counter

--- count.py
+++ count.py
@@ -2,15 +2,17 @@ import time
 
 for i in range(100000):
     file = open('/Users/ronechen/Ansible/test-projects/baselines/win_baseline/defaults/main.yml', 'r')
-    count_1 = 0
-    count_2 = 0
+    last = 0
+    count = 0
+    minute = 0
     lines = file.readlines()
     for line in lines:
-        if 'Manage' in line:
-            count_1 += 1
         if 'True' in line:
-            count_2 += 1
-
-    print(f'Number of lines containing tasks: {count_1, count_2}')
+            count += 1
+            minute = 0
+        if count == last:
+            minute += 1
+    print(f'Number of lines containing tasks: {count}, I don\'t make any changes in the last {minute} minutes.)')
+    last = count
     file.close()
     time.sleep(60)
