f52546399cf3203495a7bed260b3310260c1951f
Rone Chen <m13552079019@163.com>
2024-02-09 14:55:17 +0800

    fix: counter

--- count.py
+++ count.py
@@ -9,9 +9,10 @@ for i in range(100000):
     for line in lines:
         if 'True' in line:
             count += 1
-            minute = 0
         if count == last:
             minute += 1
+        else:
+            minute = 0
     print(f'Number of lines containing tasks: {count}, I don\'t make any changes in the last {minute} minutes.)')
     last = count
     file.close()
