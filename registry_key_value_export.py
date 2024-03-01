0ace1830be33132a9ee5fccdd10cccfbf1036d8c
Rone Chen <m13552079019@163.com>
2024-03-01 09:41:22 +0800

    test: win_baseline_9013_state

---
+++ registry_key_value_export.py
@@ -0,0 +1,19 @@
+import os
+
+file = open('/Users/ronechen/Ansible/test-projects/baselines/win_baseline/tasks/main.yml')
+lines = file.readlines()
+
+flag = False
+control_ids = []
+
+for line in lines:
+    if 'win_regedit' in line:
+        flag = True
+        continue
+    if flag:
+        control_id = line.split('_')[2]
+        print(control_id)
+        control_ids.append(control_id)
+        flag = False
+
+print(control_ids)
