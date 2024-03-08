f8ed134365fbec02bdd7982c11277e9a259e0df7
Rone Chen <m13552079019@163.com>
2024-03-08 10:37:37 +0800

    add counter

--- export_index.py
+++ export_index.py
@@ -8,10 +8,26 @@ control_ids = []
 for line in lines:
     if '- name: "win_baseline_' in line:
         control_id = line.split('_')[2].strip()
-        control_ids.append(control_id)
+        control_ids.append(int(control_id))
     
 
-print(control_ids)
+print(f'current finished tasks: {len(control_ids)}')
 
 file.close()
 
+control_ids.sort()
+
+file.close()
+
+file = open('/Users/ronechen/Ansible/test-projects/control_ids.txt')
+lines = file.readlines()
+lines = lines[0].split(',')
+exported_control_ids = [int(x.strip()) for x in lines]
+print(f'all tasks in baseline: {len(exported_control_ids)}')
+for id in control_ids:
+    try:
+        exported_control_ids.remove(id)
+    except:
+        print(id)
+print(f'left rate: {len(control_ids) / (len(exported_control_ids)+23)}%')
+print(f'lefted items: {exported_control_ids}')
