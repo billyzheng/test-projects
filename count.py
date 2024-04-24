9a90919522b6bb5758107ba6f69875e42eb63ad4
Rone Chen <m13552079019@163.com>
2024-04-24 09:35:04 +0800

    Refactor win_baseline_11024_state_Manage to win_baseline_11024_state_Manage and add win_baseline_11026_state in windows_baseline_common role tasks

--- count.py
+++ count.py
@@ -5,7 +5,7 @@ minute = 0
 last = 0
 for i in range(100000):
     # file = open('/Users/ronechen/Ansible/test-projects/baselines/win_baseline/defaults/main.yml', 'r')
-    file = open('/Users/ronechen/Ansible/test-projects/baselines/win_baseline/defaults/main.yml')
+    file = open('/Users/ronechen/Ansible/test-projects/roles/windows_baseline_common/tasks/main.yml')
     count = 0
     lines = file.readlines()
     for line in lines:
